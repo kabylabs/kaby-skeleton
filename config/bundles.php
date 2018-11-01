8353692fca64992456a6494d891c273dd36e00b7
arif <arifsetiantoo@gmail.com>
2018-11-01 12:09:55 +0700

    Add twig bundle, rest bundle & framework extra bundle

--- config/bundles.php
+++ config/bundles.php
@@ -8,4 +8,7 @@ return [
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     Bazinga\Bundle\HateoasBundle\BazingaHateoasBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
+    FOS\RestBundle\FOSRestBundle::class => ['all' => true],
 ];
